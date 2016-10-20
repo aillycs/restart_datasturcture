a67ebcf1a256e406fa324b1cfc19f4e99c358a45
aillycs <16212010033@fudan.edu.cn>
2016-10-20 21:23:21 +0800

    add new insertion_sort.c

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # restart_datasturcture
 some datastructure code 
 For the purpose of restarting my coding career.
+
+Sort algorithms:
+bubble_sort.c
+select_sort.c
+insertion_sort.c
+heap_sort.c
+quick_sort.c
--- insertion_sort.c
+++ insertion_sort.c
@@ -1,14 +1,32 @@
 /*************************************************************************
-    > File Name: restart/insertion_sort.c
-    > Author: ailly
-    > Mail: ailly@fudan.com 
-    > Created Time: 2016年10月20日 星期四 13时11分43秒
+    > File Name: insertion_sort.c
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2016年10月20日 星期四 21时51分09秒
  ************************************************************************/
 
 #include<stdio.h>
 
-int main(){
+void InsertionSort(int *L, int length);
 
-	return 0;
+int main(){
+	int a[] = {0, 10, 50, -8, 20, 11, 99, 100};
+	
+	InsertionSort(a, 7);
+
+	for (int i = 0; i < 7; ++i)
+		printf("%d ", a[i]);
+}
+
+void InsertionSort(int *L, int length){
+	for (int i = 1; i < length; i++){
+		if (L[i] < L[i - 1]){
+			int key  = L[i];
+			int j;
+			for (j = i - 1; L[j] > key && j >= 0; j--)
+				L[j + 1] = L[j];
+			L[j + 1] = key;
+		}
+	}
 }
 
